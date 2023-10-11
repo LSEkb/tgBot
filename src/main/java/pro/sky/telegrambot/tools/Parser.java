89e165752e056b73266d95193ba4f452330f2ba5
Larisa <laserova@yandex.ru>
2023-10-11 14:44:43 +0500

    ver 1

---
+++ src/main/java/pro/sky/telegrambot/tools/Parser.java
@@ -0,0 +1,25 @@
+package pro.sky.telegrambot.tools;
+
+import pro.sky.telegrambot.model.NotificationTask;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+
+    public static NotificationTask createTask(String msgTxt) {
+        Pattern pattern = Pattern.compile("(.{5})([0-9.:\\s]{16})(\\s)(.+)");
+        Matcher matcher = pattern.matcher(msgTxt);
+        NotificationTask task = new NotificationTask();
+        if (matcher.matches()) {
+            LocalDateTime dateTime = LocalDateTime.parse(matcher.group(2), DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+            task.setDateTime(dateTime);
+            task.setNotification(matcher.group(4));
+        } else {
+            throw new IllegalArgumentException("incorrect data format");
+        }
+        return task;
+    }
+}
